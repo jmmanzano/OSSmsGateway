13a2fd52563ee78c835ff4cbcf1d0654a4f17616
Jose Miguel Manzano <manzano.jm@gmail.com>
2020-07-10 19:52:31 +0200

    Sms send api response wait to complete sms send

--- app/src/main/java/es/jmmanzano/ossmsgateway/MainActivity.java
+++ app/src/main/java/es/jmmanzano/ossmsgateway/MainActivity.java
@@ -1,20 +1,16 @@
 package es.jmmanzano.ossmsgateway;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.text.format.Formatter;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            httpServer = new HttpServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logTextView = (TextView) findViewById(R.id.logTextView);
@@ -58,57 +59,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
-        //---when the SMS has been sent---
-        registerReceiver(new BroadcastReceiver(){
+        Button btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        addLog("SMS SEND ");
-                        Toast.makeText(getBaseContext(), "SMS sent",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(getBaseContext(), "Generic failure",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(getBaseContext(), "No service",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(getBaseContext(), "Null PDU",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(getBaseContext(), "Radio off",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
+            public void onClick(View v) {
+                logTextView.setText("");
             }
-        }, new IntentFilter(SENT));
-
-        //---when the SMS has been delivered---
-        registerReceiver(new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        addLog("SMS DELIVERED");
-                        Toast.makeText(getBaseContext(), "SMS delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(getBaseContext(), "SMS not delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(DELIVERED));
+        });
         // static variable for context into handler
         ma = this;
     }
@@ -119,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         boolean perm2 = Boolean.FALSE;
         checkPermissions(Manifest.permission.READ_SMS, MY_PERMISSIONS_REQUEST_READ_SMS, PERMISSIONS_READ_SMS);
         try {
-            httpServer = new HttpServer();
+
             httpServer.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
         } catch (IOException e) {
             e.printStackTrace();
--- app/src/main/java/es/jmmanzano/ossmsgateway/handler/v1/sms/SmsHandler.java
+++ app/src/main/java/es/jmmanzano/ossmsgateway/handler/v1/sms/SmsHandler.java
@@ -1,11 +1,15 @@
 package es.jmmanzano.ossmsgateway.handler.v1.sms;
 
+import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.telephony.SmsManager;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +22,69 @@ import fi.iki.elonen.router.RouterNanoHTTPD;
 
 
 public class SmsHandler extends RouterNanoHTTPD.DefaultHandler {
-    public static final String MIME_PLAINTEXT = "text/plain";
-    private static final String SENT_SMS_FLAG = "SENT_SMS";
+    private static String SENT = "SMS_SENT";
+    private static String DELIVERED = "SMS_DELIVERED";
+    private static ArrayList<Integer> listaSmsStatus = new ArrayList<>();
+    private BroadcastReceiver brSender = null;
+    private BroadcastReceiver brDelivered = null;
+
+    public SmsHandler(){
+        this.brSender = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        MainActivity.addLog("SMS SEND ");
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                listaSmsStatus.add(getResultCode());
+            }
+        };
+
+        this.brDelivered = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        MainActivity.ma.addLog("SMS DELIVERED");
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(MainActivity.ma.getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+
+
+        //---when the SMS has been sent---
+        MainActivity.ma.registerReceiver(brSender, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        MainActivity.ma.registerReceiver(brDelivered, new IntentFilter(DELIVERED));
+    }
 
     @Override
     public String getText() {
@@ -37,7 +102,6 @@ public class SmsHandler extends RouterNanoHTTPD.DefaultHandler {
     }
 
     public NanoHTTPD.Response get(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
-        MainActivity.addLog("Request to: "+uriResource.toString()+" with method "+session.getMethod());
         if (session.getMethod() == NanoHTTPD.Method.GET && uriResource.getUri().toString().equals("v1/sms/send") ||
                 session.getMethod() == NanoHTTPD.Method.POST) {
             Map<String, String> params = new HashMap<String, String>();
@@ -69,12 +133,34 @@ public class SmsHandler extends RouterNanoHTTPD.DefaultHandler {
                 sentPendingIntents.add(i, sentPI);
                 deliveredPendingIntents.add(i, deliveredPI);
             }
+            listaSmsStatus = new ArrayList<>();
             smsManager.sendMultipartTextMessage(phone, null, smsParts, sentPendingIntents, deliveredPendingIntents);
-            for (PendingIntent pi: sentPendingIntents ) {
-
-            };
-
             MainActivity.addLog("Message send to :" + phone + " " + message);
+            // retry is used to define a timeout.
+            // if we need 100 iterations return status 500.
+            int retry = 0;
+            do {
+                retry++;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }while (smsParts.size() > listaSmsStatus.size() || retry == 100);
+            //
+            //-- unregister receivers
+            MainActivity.ma.unregisterReceiver(brSender);
+            MainActivity.ma.unregisterReceiver(brDelivered);
+            //-------------------------------------
+            if(retry == 100){
+                return Utils.getResponse("{\"status\":500}", "", true);
+            }
+
+            for (Integer value: listaSmsStatus) {
+                if(value != Activity.RESULT_OK ){
+                    return Utils.getResponse("{\"status\":500}", "", true);
+                }
+            }
             return Utils.getResponse("{\"status\":200}", "", true);
         }else if((session.getMethod() == NanoHTTPD.Method.GET && uriResource.getUri().toString().equals("v1/sms"))
                 ||(session.getMethod() == NanoHTTPD.Method.GET && uriResource.getUri().toString().equals("v1/sms/:limit") )){
